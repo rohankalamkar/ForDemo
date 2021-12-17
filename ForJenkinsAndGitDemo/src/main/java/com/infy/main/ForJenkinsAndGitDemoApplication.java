1b2e407eb63c376c0b25e0377a71bcbc60da099d
rohankalamkar <93188315+rohankalamkar@users.noreply.github.com>
2021-12-17 20:56:23 +0530

    added sop

--- ForJenkinsAndGitDemo/src/main/java/com/infy/main/ForJenkinsAndGitDemoApplication.java
+++ ForJenkinsAndGitDemo/src/main/java/com/infy/main/ForJenkinsAndGitDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ForJenkinsAndGitDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello This is demo...");
 		SpringApplication.run(ForJenkinsAndGitDemoApplication.class, args);
 	}
 
